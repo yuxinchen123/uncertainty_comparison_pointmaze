4f1ef2a8866195a39b48ed32a74d17f73edbd55d
sl5nw <sl5nw@virginia.edu>
2025-09-09 02:16:39 -0400

    save max_env notebook

--- sweep_uncertainty/slurm/00_batch_slurm.sh
+++ sweep_uncertainty/slurm/00_batch_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Define two variables with name job_id and cpu_core
-job_id="uncertainty_comparison/c4q75qxx"
+job_id="uncertainty_comparison_pointmaze/cnt87hy4"
 
 # for i in $(seq 1 1); do
 #     sbatch slurm/01_run_gpu.slurm $job_id&
