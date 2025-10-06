311e332b6069f98e2c0309058e78e0f0ab4841ee
sl5nw <sl5nw@virginia.edu>
2025-10-06 03:00:43 -0400

    rnd linear sgd slurm implemented

--- sweep_uncertainty/slurm/00_batch_slurm.sh
+++ sweep_uncertainty/slurm/00_batch_slurm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #Define two variables with name job_id and cpu_core
-job_id="rnd_linear_pointmaze/zvevykr3"
+job_id="rnd_linear_sgd_pointmaze/tp4szwp8"
 
 # for i in $(seq 1 1); do
 #     sbatch slurm/01_run_gpu.slurm $job_id&
