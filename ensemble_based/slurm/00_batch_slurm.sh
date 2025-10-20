c92f60271946a2c4e17cbc331633616d46390b90
sl5nw <sl5nw@virginia.edu>
2025-10-20 02:01:52 -0400

    Update ensemble sweep configurations to reduce num_averaging_runs to 1 and modify phi_seed values for RND methods. Adjust SLURM batch script to use a new job_id and enable multiple job submissions for GPU, gnolim, and CPU runs.

--- ensemble_based/slurm/00_batch_slurm.sh
+++ ensemble_based/slurm/00_batch_slurm.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 #Define two variables with name job_id and cpu_core
-job_id="RND-ensemble_based/3kc4m7ip"
+job_id="RND-ensemble_based/akq8twsl"
 
-for i in $(seq 1 1); do
-    sbatch slurm/01_run_gpu.slurm $job_id&
-done
-
-
-# for i in $(seq 1 5); do
+# for i in $(seq 1 1); do
 #     sbatch slurm/01_run_gpu.slurm $job_id&
 # done
 
-# for i in $(seq 1 5); do
-#     sbatch slurm/02_run_gnolim.slurm $job_id&
-# done
 
-# for i in $(seq 1 5); do
-#     sbatch slurm/03_run_cpu.slurm $job_id&
-# done
+for i in $(seq 1 5); do
+    sbatch slurm/01_run_gpu.slurm $job_id&
+done
+
+for i in $(seq 1 5); do
+    sbatch slurm/02_run_gnolim.slurm $job_id&
+done
+
+for i in $(seq 1 5); do
+    sbatch slurm/03_run_cpu.slurm $job_id&
+done
 
 
 
